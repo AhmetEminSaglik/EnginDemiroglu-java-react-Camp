2498d9c58afe40cca1adda239d9b98326777a3fa
Ahmet_Emin <ahmeteminsaglik@gmail.com>
2022-09-07 08:53:58 +0300

    started react

---
+++ reactcamp/camp-project/src/layouts/Dashboard.js
@@ -0,0 +1,14 @@
+//rfc yazinca otomatik React function gelir
+import React from 'react'
+import Navi from  './Navi'
+import Categories from  './Categories'
+import ProductList from '../pages/ProductList'
+export default function Dashboard() {
+    return (
+        <div>
+            <Navi/>
+            <Categories/>
+            <ProductList/>            
+     </div>
+    )
+}
